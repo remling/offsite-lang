14dfd09e005c8bbb85d4a58dcca11c926d3b57a5
Brian Samek <brian.samek@mongodb.com>
2018-10-16 15:31:26 -0400

    moar auth

--- lang.js
+++ lang.js
@@ -2,6 +2,10 @@ client = stitch.Stitch.initializeDefaultAppClient('offsite-language-app-oubno');
 db = client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('lang');
 
 function displayOnLoad() {
+  client.auth
+    .loginWithCredential(new stitch.AnonymousCredential())
+    .catch(console.error);
+
   db.collection('strings')
     .find({}, { limit: 100 })
     .asArray()
@@ -11,6 +15,10 @@ function displayOnLoad() {
 }
 
 function addString(string) {
+  client.auth
+    .loginWithCredential(new stitch.AnonymousCredential())
+    .catch(console.error);
+
   db.collection("strings")
     .insertOne({content: document.getElementById("new_string").value})
     .catch(err => console.error(err));
