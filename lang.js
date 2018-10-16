b443b36d200eb2e99e371273847bb2029766c6ac
Francisco Alanis <francisco.alanis@mongodb.com>
2018-10-16 15:17:25 -0400

    Translated word insert functionality

--- lang.js
+++ lang.js
@@ -27,10 +27,41 @@ function addString(string) {
     .catch(err => console.error(err));
 }
 
+function addString(string) {
+    client = getClient();
+    db = getDB(client);
+  
+    client.auth
+      .loginWithCredential(new stitch.AnonymousCredential())
+      .then(() => db.collection("strings").insertOne({content: string}))
+      .catch(err => console.error(err));
+}
+
+function addWord(word) {
+    client = getClient();
+    db = getDB(client);
+  
+    client.auth
+      .loginWithCredential(new stitch.AnonymousCredential())
+      .then(() => db.collection("words").insertOne(word))
+      .catch(err => console.error(err));
+}
+
 function getClient() {
     return stitch.Stitch.initializeDefaultAppClient('offsite-language-app-oubno');
 }
 
 function getDB(client) {
     return client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('lang');
+}
+
+function getWordToSave() {
+    textbox = document.getElementById('paragraph')
+    trinput = document.getElementById('selectionTranslation')
+    t = textbox.value.substr(textbox.selectionStart, textbox.selectionEnd - textbox.selectionStart);
+    word = {
+        'from': 
+    }
+    addWord()
+    console.log(t)
 }
