f3e947e13d5641be8e9177631c4f30e90f773a09
Brian Samek <brian.samek@mongodb.com>
2018-10-16 14:53:55 -0400

    Fixup map

--- lang.js
+++ lang.js
@@ -12,7 +12,7 @@ function displayStrings() {
   db.collection('strings')
     .find({}, { limit: 100 })
     .asArray()
-    .then(docs => docs.map(doc => `<div>${doc.content}</div>`))
+    .then(docs => docs.map(doc => `<div>${doc.content}</div>`).join(''))
     .then(strings => document.getElementById("strings").innerHTML = strings);
 }
 
