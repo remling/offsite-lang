6693897cad18def9ecbf8918da15e7401423bbac
Brian Samek <brian.samek@mongodb.com>
2018-10-16 14:45:50 -0400

    Get strings from db

---
+++ lang.js
@@ -0,0 +1,25 @@
+function displayOnLoad() {
+    client = getClient();
+    db = getDB(client);
+
+    client.auth
+        .loginWithCredential(new stitch.AnonymousCredential())
+        .then(displayParagraphs)
+        .catch(console.error);
+}
+
+function displayParagraphs() {
+  db.collection('strings')
+    .find({}, { limit: 100 })
+    .asArray()
+    .then(docs => docs.map(doc => `<div>${doc.content}</div>`))
+    .then(strings => document.getElementById("strings").innerHTML = strings);
+}
+
+function getClient() {
+    return stitch.Stitch.initializeDefaultAppClient('offsite-language-app-oubno');
+}
+
+function getDB(client) {
+    return client.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('lang');
+}
